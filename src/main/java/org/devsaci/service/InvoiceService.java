b8299eaa9ccf66412b68187d62bd0a759e2f1d62
saci zakaria <sacideveloppeur@gmail.com>
2020-07-23 13:22:42 +0200

    instanciation :invoiceRepository.create(invoice);

--- src/main/java/org/devsaci/service/InvoiceService.java
+++ src/main/java/org/devsaci/service/InvoiceService.java
@@ -1,10 +1,16 @@
 package org.devsaci.service;
 
 import org.devsaci.entity.Invoice;
+import org.devsaci.repository.InvoiceRepository;
 
 public class InvoiceService {
     private static long lastNumber=0L;
+
+private InvoiceRepository invoiceRepository=new InvoiceRepository();
+
+
     public void createInvoice(Invoice invoice){
         invoice.setNumber(String.valueOf(++lastNumber));
+        invoiceRepository.create(invoice);
     }
 }
