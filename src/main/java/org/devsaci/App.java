da9036aa6b51202d9d21367c5cfbd2cd3b779197
saci zakaria <sacideveloppeur@gmail.com>
2020-07-23 13:32:31 +0200

    public static void main

--- src/main/java/org/devsaci/App.java
+++ src/main/java/org/devsaci/App.java
@@ -1,5 +1,10 @@
 package org.devsaci;
 
+import org.devsaci.entity.Invoice;
+import org.devsaci.service.InvoiceService;
+
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+
+        System.out.println("What is the customer name?"  );
+        Scanner sc=new Scanner(System.in);
+        String customerNam=sc.nextLine();
+        Invoice invoice=new Invoice();
+        invoice.setCustomerName(customerNam);
+        InvoiceService invoiceService=new InvoiceService();
+        invoiceService.createInvoice(invoice);
+
     }
 }
