92dad7bfbfe5e69caffb52d1dc51aaff0bc19f82
saci zakaria <sacideveloppeur@gmail.com>
2020-07-29 12:24:00 +0200

    Garage Michel

---
+++ src/main/java/org/devsaci/service/InvoiceServiceMichel.java
@@ -0,0 +1,16 @@
+package org.devsaci.service;
+
+import org.devsaci.entity.Invoice;
+import org.devsaci.repository.InvoiceRepository;
+
+public class InvoiceServiceMichel {
+    private static long lastNumber=0L;
+
+private InvoiceRepository invoiceRepository=new InvoiceRepository();
+
+
+    public void createInvoice(Invoice invoice){
+        invoice.setNumber(String.valueOf(++lastNumber));
+        invoiceRepository.create(invoice);
+    }
+}
